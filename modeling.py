44919eaf63850747c3e6cbc1950f3b9e272ae195
moa000 <21integral@gmail.com>
2024-01-30 12:09:51 +0900

    file changed

--- modeling.py
+++ modeling.py
@@ -4,21 +4,23 @@ from tensorflow.keras.models import *
 from tensorflow.keras.layers import *
 
 X_train, X_test, Y_train, Y_test = np.load(
-    './news_data_max_18_wordsize_7524.npy', allow_pickle=True
+    '../naver_economy/news_data_max_20_wordsize_8607.npy', allow_pickle=True
 )
 print(X_train.shape, Y_train.shape)
 print(X_test.shape, Y_test.shape)
 
 model = Sequential()
-model.add(Embedding(7524, 300, input_length=18))
+model.add(Embedding(8607, 300, input_length=20))
 model.add(Conv1D(32, kernel_size=5, padding='same', activation='relu'))
 model.add(MaxPooling1D(pool_size=1))
+
 model.add(LSTM(128, activation='tanh', return_sequences=True))
 model.add(Dropout(0.3))
 model.add(LSTM(64, activation='tanh', return_sequences=True))
 model.add(Dropout(0.3))
 model.add(LSTM(64, activation='tanh'))
 model.add(Dropout(0.3))
+
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dense(8, activation='softmax'))
@@ -26,7 +28,9 @@ model.summary()
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 fit_hist = model.fit(X_train, Y_train, batch_size=128, epochs=10, validation_data=(X_test, Y_test))
-model.save('./models/news_category_classification_model_{}.h5'.format(fit_hist.history['val_accuracy'][-1]))
+#model.save('./economy_category_classification_model_{}.h5'.format(fit_hist.history['val_accuracy'][-1]))
+
+model.save('../naver_economy/economy_category_classification_model_{}.h5'.format(fit_hist.history['val_accuracy'][-1]))
 plt.plot(fit_hist.history['val_accuracy'], label='val_accuracy')
 plt.plot(fit_hist.history['accuracy'], label='train_accuracy')
 plt.legend()
